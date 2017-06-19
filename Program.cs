e8696557f9544380e03829700598a14b0c8907e3
nielsthedane <nmlthedane@gmail.com>
2017-06-20 00:57:14 +0200

    Added MemoryManagementClass
    Class Borrowed from C0reExternal-Base

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LZTime.MMC;
+
+namespace LZTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MMC.MMCManage.Initialize("csgo");
+        }
+    }
+}
