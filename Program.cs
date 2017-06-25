b06cec36f5d1085b957207a99f3eb268c4d47132
nielsthedane <nmlthedane@gmail.com>
2017-06-25 02:26:37 +0200

    Simple triggerbot is working, shoots at everything. Health and team needs to be checked before shooting.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static LZTime.MMC;
 
 namespace LZTime
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            MMC.MMCManage.Initialize("csgo");
-        }
-    }
+				class Program
+				{
+								//Offsets
+								public static int dwLocalPlayer = 0xAADFFC;
+								public static int dwForceAttack = 0x2ECCA08;
+
+								public static int m_iCrosshairId = 0xB2B4;
+
+								//Client.dll offset
+								public static int m_ClientPointer;
+
+
+								static void Main(string[] args)
+								{
+												MMC.MMCManage.Initialize("csgo");
+												//Finds Client.dll addres and saves it in the variable
+												m_ClientPointer= MMCManage.GetModuleAdress("client.dll");
+
+												
+									while (true)
+									{
+
+												int address = m_ClientPointer + dwLocalPlayer;
+												int localPlayer = MMCManage.ReadMemory<int>(address);
+
+										address = localPlayer + m_iCrosshairId;
+										int playerInCross = MMCManage.ReadMemory<int>(address);
+										Console.WriteLine("Player" + playerInCross);
+										if (playerInCross == 2)
+										{
+											MMCManage.WriteMemory<IntPtr>(m_ClientPointer + dwForceAttack ,1);
+											Thread.Sleep(1);
+											MMCManage.WriteMemory<IntPtr>(m_ClientPointer + dwForceAttack, 4);
+																}
+									}
+								}
+				}
 }
